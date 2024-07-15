428a5589aef179ae8932c11918bf88bc9945cba7
Vitaly Smirnov <mrmaybelately@gmail.com>
2024-07-15 19:38:58 +0300

    Add quiz detail

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, QuizListView
+from .views import index, QuizListView, QuizView
 
 app_name = 'quizzes'
 
 urlpatterns = [
     # path('', index, name='index'),
     path('', QuizListView.as_view(), name='quiz_list'),#name='index'),
+    path('quiz/<int:pk>', QuizView.as_view(), name='quiz'),
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,8 +1,9 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from .models import Quiz
+from .models import Quiz, Question, Answer
 
 # Create your views here.
 
@@ -13,3 +14,31 @@ def index(request):
 class QuizListView(ListView):
     model = Quiz
     template_name = 'quizzes/quiz_list.html'
+
+
+class QuizView(DetailView):#ListView):
+    model = Quiz  #Question
+    template_name = 'quizzes/quiz.html'
+    # context_object_name = 'questions'
+
+    # def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        # return super().get_context_data(**kwargs)
+        # quizzes = super().get_context_data(**kwargs)
+        # for v in kwargs:
+        #     print(v)
+        # q = kwargs['object']
+        # print(kwargs['object'])
+        # print(quizzes)
+        # print(type(q).__dict__)# .objects.all())
+
+        # print(self.object.questions.all())
+        # for o in self.object.questions.all():
+        #     print(o)
+
+        # return self.object.questions.all() #quizzes
+        
+        # context = super().get_context_data(**kwargs)
+        # context['questions'] = self.object.questions.all()
+        # context['answers'] = None
+
+        # return context
