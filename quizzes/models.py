ec97da3acbf8d23db1fa7dcad1b7f005bba00ce1
Vitaly Smirnov <mrmaybelately@gmail.com>
2024-07-15 17:33:10 +0300

    Going to apply quiz models migrations..still

--- quizzes/models.py
+++ quizzes/models.py
@@ -6,6 +6,7 @@ class Quiz(models.Model): ...
 class Question(models.Model): ...
 class Answer(models.Model): ...
 
+
 class Quiz(models.Model):
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=512)
@@ -16,14 +17,31 @@ class Question(models.Model):
     test = models.ForeignKey(to=Quiz, on_delete=models.CASCADE,
                              related_name='questions')
     # right_answer = models.ForeignKey(to=Answer, on_delete=models.CASCADE,
-    #                                  related_name='questions')
+    #                                  related_name='question2')
+    # right_answer = models.OneToOneField(to='Answer', on_delete=models.CASCADE,
+    #                                     related_name='right_for_question')
     # right_answer = models.ForeignKey(to='quizzes.Answer', on_delete=models.CASCADE,
     #                                  related_name='questions')
+    # right_answer = models.IntegerField()
     price = models.IntegerField(default=0)
 
 
 class Answer(models.Model):
     text = models.CharField(max_length=512)
-    question = models.ForeignKey(to=Question, on_delete=models.CASCADE,
+    question = models.ForeignKey(to='Question', on_delete=models.CASCADE,
                                  related_name='answers')
+    
 
+class QuestionAnswer(models.Model):
+    """ Right answers for each question """
+    question = models.OneToOneField(to=Question, on_delete=models.CASCADE,
+                                    related_name='right_answer')
+    answer = models.ForeignKey(to=Answer, on_delete=models.CASCADE,
+                               related_name='questions')
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('question', 'answer'),
+                name='right_answers'
+            ),
+        )
