ffde44ed5fb14ca386c4827fd65299205cb06af5
Vitaly Smirnov <mrmaybelately@gmail.com>
2024-07-15 15:10:45 +0300

    Going to apply quiz models migrations

--- quizzes/models.py
+++ quizzes/models.py
@@ -4,20 +4,26 @@ from django.db import models
 # Models list:
 class Quiz(models.Model): ...
 class Question(models.Model): ...
-class Answer(models.Models): ...
+class Answer(models.Model): ...
 
 class Quiz(models.Model):
-    title = models.CharField()
-    description = models.CharField()
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=512)
 
 
 class Question(models.Model):
-    text = models.CharField()
-    test = models.ForeignKey(to=Quiz)
-    right_answer = models.ForeignKey(to=Answer, related_name='questions')
+    text = models.CharField(max_length=512)
+    test = models.ForeignKey(to=Quiz, on_delete=models.CASCADE,
+                             related_name='questions')
+    # right_answer = models.ForeignKey(to=Answer, on_delete=models.CASCADE,
+    #                                  related_name='questions')
+    # right_answer = models.ForeignKey(to='quizzes.Answer', on_delete=models.CASCADE,
+    #                                  related_name='questions')
     price = models.IntegerField(default=0)
 
 
 class Answer(models.Model):
-    text = models.CharField()
-    question = models.ForeignKey(to=Question, related_name='answers')
+    text = models.CharField(max_length=512)
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE,
+                                 related_name='answers')
+
--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from ..quizzes.models import Quiz
+from quizzes.models import Quiz
 
 # from colorfield.fields import ColorField
 
