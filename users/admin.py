283311b82e2eb65ddb67655c5768abefc2e85488
Vitaly Smirnov <mrmaybelately@gmail.com>
2024-07-19 22:14:32 +0300

    Some minor update

--- users/admin.py
+++ users/admin.py
@@ -2,30 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 
-from .models import User, Color
+from .models import Color
 
 
-# admin.site.register(User, UserAdmin)
-admin.site.register(get_user_model())#, UserAdmin)
+admin.site.register(get_user_model())
 admin.site.register(Color)
-
-
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'username', 'email', 'is_active', 'last_login', 'is_staff', )
-#     readonly_fields = (
-#         'password', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name',
-#         'email', 'balance', 'color', 'passed_tests', 'date_joined', 'username',
-#     )
-#     empty_value_display = VALUE_DISPLAY
-
-
-# @admin.register(get_user_model())
-# class NewUserAdmin(UserAdmin):
-#     # pass
-#     list_display = ('id', 'username', 'email', 'is_active', 'last_login', 'is_staff', )
-#     readonly_fields = (
-#         'password', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name',
-#         'email', 'balance', 'color', 'passed_tests', 'date_joined', 'username',
-#     )
-#     # empty_value_display = VALUE_DISPLAY
